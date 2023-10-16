968914194e1f194ea5114b44876ccafbe75c0798
chihiroloh <lohchihiro@gmail.com>
2023-10-16 11:44:22 +0800

    updated changes

--- script.js
+++ script.js
@@ -8,21 +8,31 @@ document.querySelector("h1").addEventListener("click", function (e) {
   e.target.innerHTML = "";
 });
 
-const timeleft = 10;
-const downloadTimer = setInterval(function () {
-  if (timeleft <= 0) {
-    clearInterval(downloadTimer);
-    document.getElementById("countdown").innerHTML = "Finished";
-  } else {
-    document.getElementById("countdown").innerHTML = timeleft;
-  }
-  timeleft -= 1;
-}, 1000);
+let timeleft = 15;
+let downloadTimer;
+
+function startTimer() {
+  clearInterval(downloadTimer);
+  timeleft = 15;
+  downloadTimer = setInterval(function () {
+    if (timeleft <= 0) {
+      clearInterval(downloadTimer);
+      const countdownElem = document.getElementById("countdown");
+      countdownElem.innerHTML = "GAME OVER";
+      countdownElem.classList.add("game-over");
+    } else {
+      document.getElementById("countdown").innerHTML = timeleft;
+    }
+    timeleft -= 1;
+  }, 1000);
+}
+
+document.querySelector(".play").addEventListener("click", startTimer);
 
 let currMoleTile;
 let score = 0;
 let gameOver = false;
 
 window.onload = function () {
-  setGame();
+  // setGame(); // If you have a function called setGame, you can call it here
 };
